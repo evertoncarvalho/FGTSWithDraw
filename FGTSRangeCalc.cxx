1231b74124fe6bdfa8972ffc3bb8eef10fda2804
Everton C. Silva <everton.emails@gmail.com>
2019-08-14 21:04:36 -0300

    Correcting the FGTSRules routine.
    The problem was that the input was not evaluated correctly.
    Improvements was made in the automated test: Now we're using a helper
    macro in CMakeLists.txt.

--- FGTSRangeCalc.cxx
+++ FGTSRangeCalc.cxx
@@ -15,38 +15,41 @@ namespace FGTSWithdraw
 		       	*outWithdrawAliquot = WITHDRAW_ALIQUOT_1;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_1;
 		}
-		if (currentBalance <= BALANCE_RANGE_2)
+		else if (currentBalance <= BALANCE_RANGE_2)
 		{
 			*outRange = 2;
 			*outWithdrawAliquot = WITHDRAW_ALIQUOT_2;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_2;
 		}
-		if (currentBalance <= BALANCE_RANGE_3)
+		else if (currentBalance <= BALANCE_RANGE_3)
 		{
 			*outRange = 3;
 			*outWithdrawAliquot = WITHDRAW_ALIQUOT_3;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_3;
 		}
-		if (currentBalance <= BALANCE_RANGE_4)
+		else if (currentBalance <= BALANCE_RANGE_4)
 		{
 			*outRange = 4;
 			*outWithdrawAliquot = WITHDRAW_ALIQUOT_4;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_4;
 		}
-		if (currentBalance <= BALANCE_RANGE_5)
+		else if (currentBalance <= BALANCE_RANGE_5)
 		{
 			*outRange = 5;
 			*outWithdrawAliquot = WITHDRAW_ALIQUOT_5;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_5;
 		}
-		if (currentBalance <= BALANCE_RANGE_6)
+		else if (currentBalance <= BALANCE_RANGE_6)
 		{
 			*outRange = 6;
 			*outWithdrawAliquot = WITHDRAW_ALIQUOT_6;
 			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_6;
 		}
-		*outRange = 7;
-		*outWithdrawAliquot = WITHDRAW_ALIQUOT_7;
-		*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_7;
+		else
+		{
+			*outRange = 7;
+			*outWithdrawAliquot = WITHDRAW_ALIQUOT_7;
+			*outAdditionalWithdraw = ADDITIONAL_WITHDRAW_7;
+		}
 	}
 };
--- main.cxx
+++ main.cxx
@@ -4,8 +4,21 @@
 
 using namespace FGTSWithdraw;
 
+void printUsage()
+{
+	std::cout << "You need to pass your current balance in FGTS account, something like:\n"
+		<< " ./FGTSWithdraw 10000"
+		<< std::endl;
+}
+
 int main (int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		printUsage();
+		return 0;
+	}
+
 	FGTSRangeCalc calculator;
 	std::cout << "versao " << FGTSWithdraw_VERSION_MAJOR
 	      << "." << FGTSWithdraw_VERSION_MINOR << std::endl;
@@ -13,8 +26,8 @@ int main (int argc, char *argv[])
 	double withdrawAliquot, additionalWithdraw;
 	calculator.getWithdrawRule(atof(argv[1]), &withdrawAliquot, &additionalWithdraw,&range);
 	std::cout << "For balance of " << argv[1]// << "\n"
-		<< "Aliquot: " << withdrawAliquot// << "\n"
-		<< "Additional Withdraw: " << additionalWithdraw// << "\n"
+		<< " Aliquot: " << withdrawAliquot// << "\n"
+		<< " Additional Withdraw: " << additionalWithdraw// << "\n"
 		<< " range " << range << std::endl;
 	return 0;
 }
